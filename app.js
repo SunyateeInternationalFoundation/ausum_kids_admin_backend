a2fd392f846c2842de77952b41cd7d41f758f15d
mutant138 <udhayasurya138@gmail.com>
2024-12-31 16:17:37 +0530

    login_controller_done

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
+require('dotenv').config(); 
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const adminRoutes = require('./routes/admin');
+app.use(cors());
+app.use(express.json());
 
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+app.use('/', adminRoutes)
 
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+
+mongoose.connect(process.env.MONGO_URI, {
+    useUnifiedTopology: true,
+  })
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((err) => console.error('Database connection error:', err));
+
+app.listen(5000, () => {
+    console.log('Server is running on http://localhost:5000');
 });
---
+++ models/admin.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const adminSchema = new Schema({
+  email:{
+    type: String,
+    required: true,
+    unique: true
+  },
+  password:{
+    type: String,
+    required: true
+  },
+  isSuperAdmin:{
+    type: Boolean,
+    default: false
+  },
+})
+
+
+const Admins = mongoose.model('admins', adminSchema);
+
+module.exports = Admins;
+
