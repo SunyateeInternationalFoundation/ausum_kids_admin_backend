6eb10e82fdd2a708102e35f6ece1c9526a087a5d
Kayathri-maths <kayualagarsamy@gmail.com>
2024-12-31 17:52:25 +0530

    fixed_err

--- app.js
+++ app.js
@@ -1,23 +1,22 @@
-require('dotenv').config(); 
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const cors = require('cors');
+const mongoose = require("mongoose");
+const cors = require("cors");
 
-const adminRoutes = require('./routes/admin');
+const adminRoutes = require("./routes/admin");
 app.use(cors());
 app.use(express.json());
 
+app.use("/", adminRoutes);
 
-app.use('/', adminRoutes)
-
-
-mongoose.connect(process.env.MONGO_URI, {
+mongoose
+  .connect(process.env.MONGO_URI, {
     useUnifiedTopology: true,
   })
-    .then(() => console.log('Connected to MongoDB'))
-    .catch((err) => console.error('Database connection error:', err));
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Database connection error:", err));
 
 app.listen(5000, () => {
-    console.log('Server is running on http://localhost:5000');
+  console.log("Server is running on http://localhost:5000");
 });
--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,42 +1,48 @@
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv");
-const  Admin  = require('../models/admin');
+const Admin = require("../models/admin");
 dotenv.config();
 
 function isStringInvalid(string) {
-    return string === undefined || string.length === 0;
-  }
+  return string === undefined || string.length === 0;
+}
 
 const login = async (req, res) => {
-    try{
-        const { email,password } = req.body;
-        console.log(`email: ${email} password: ${password}`)
-        if(isStringInvalid(email) || isStringInvalid(password)){
-          return res.status(400).json({success: false, message: `Email and password is missing`})
-        }
-        const user = await Admin.findOne({email})
-        if(!user){
-          return res.status(400).json({success: false, message: `Kindly contact admins to create an account`})
-        }
-        if (user.password !== password) {
-          return res.status(401).json({
-              success: false, 
-              message: `Invalid email or password`
-          });
-      }
-        else{
-            res.status(200).json({
-              success: true,
-               message:`User Logged in succesfully`, 
-        })
-        }
-        }catch(err){
-          res.status(400).json({message: err, success: false})
-        }
-  };
-
-
-  module.exports = {
-    login
+  try {
+    const { email, password } = req.body;
+    console.log(`email: ${email} password: ${password}`);
+    if (isStringInvalid(email) || isStringInvalid(password)) {
+      return res
+        .status(400)
+        .json({ success: false, message: `Email and password is missing` });
+    }
+    const user = await Admin.findOne({ email });
+    if (!user) {
+      return res
+        .status(400)
+        .json({
+          success: false,
+          message: `Kindly contact admins to create an account`,
+        });
+    }
+    if (user.password !== password) {
+      return res.status(401).json({
+        success: false,
+        message: `Invalid email or password`,
+      });
+    } else {
+      res.status(200).json({
+        admin: user,
+        success: true,
+        message: `User Logged in succesfully`,
+      });
+    }
+  } catch (err) {
+    res.status(400).json({ message: err, success: false });
   }
+};
+
+module.exports = {
+  login,
+};
